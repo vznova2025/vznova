8d3585e04a7392021cb8c9a1099d89c08b8bc790
vznova2025 <harwnalrasheed@gmail.com>
2025-06-15 23:21:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('مرحبًا بك في Vznova');
+
+document.getElementById("signup-tab").addEventListener("click", function () {
+  document.getElementById("signup-form").classList.add("active");
+  document.getElementById("login-form").classList.remove("active");
+  this.classList.add("active");
+  document.getElementById("login-tab").classList.remove("active");
+});
+document.getElementById("login-tab").addEventListener("click", function () {
+  document.getElementById("signup-form").classList.remove("active");
+  document.getElementById("login-form").classList.add("active");
+  this.classList.add("active");
+  document.getElementById("signup-tab").classList.remove("active");
+});

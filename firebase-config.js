3a7b929616bca2eca3a5be8c5adb1aaf16fad8aa
vznova2025 <harwnalrasheed@gmail.com>
2025-06-15 03:19:49 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCxhcWhSKvjXFmt6grD43zu7sqs-wy_xeY",
   authDomain: "vznova-d6497.firebaseapp.com",
   projectId: "vznova-d6497",
-  storageBucket: "vznova-d6497.appspot.com",
+  storageBucket: "vznova-d6497.firebasestorage.app",
   messagingSenderId: "608106047555",
   appId: "1:608106047555:web:97176785c08738ca3e8cf6",
   measurementId: "G-NLE2Y9SNFS"
 };
-
-firebase.initializeApp(firebaseConfig);
---
+++ script.js
@@ -0,0 +1,1 @@
+console.log('مرحبًا بك في Vznova');

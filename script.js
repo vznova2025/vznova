ecea5c299fa95557f8867ea9aa598ff92e252240
vznova2025 <harwnalrasheed@gmail.com>
2025-06-16 03:25:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
-document.getElementById("signup-tab").addEventListener("click", function () {
-  document.getElementById("signup-form").classList.add("active");
-  document.getElementById("login-form").classList.remove("active");
-  this.classList.add("active");
-  document.getElementById("login-tab").classList.remove("active");
-});
-document.getElementById("login-tab").addEventListener("click", function () {
-  document.getElementById("signup-form").classList.remove("active");
-  document.getElementById("login-form").classList.add("active");
-  this.classList.add("active");
-  document.getElementById("signup-tab").classList.remove("active");
-});
+function toggleForm(type) {
+  document.getElementById("signup").classList.remove("active");
+  document.getElementById("login").classList.remove("active");
+  document.getElementById(type).classList.add("active");
+}
+
+function redirectToDashboard() {
+  window.location.href = "dashboard.html";
+}
